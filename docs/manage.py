d2af60a070ca58b59d911700b9a0aa4bc656c094
Nicolas M <miller.nicolasw@gmail.com>
2021-02-10 13:19:50 -0800

    i dont even know whats going on anymore

--- docs/manage.py
+++ docs/manage.py
@@ -1,4 +1,5 @@
 from utils import *
+import sys
 
 print('''Usage: 
     Rebuild site:    python manage.py build
@@ -8,7 +9,6 @@ prompt = input('Please choose a command: ')
 if prompt == 'python manage.py build':
     main()
 elif prompt == 'python manage.py new':
-    #function to build new page
-    main()
+    create_new()
 else:
     print("Neither command chosen. Run 'manage.py' file again if you would like to build or create new file.")
--- docs/utils.py
+++ docs/utils.py
@@ -17,7 +17,7 @@ def build_list(pages):
         })
 
 #This function uses jinja2 to loop through the pages list created from the 'build_list' function and render
-# the results to the web pages. It also creates a loop to update the index.
+# the results to the web pages.
 
 def build_pages(pages):        
     from jinja2 import Template
@@ -25,16 +25,31 @@ def build_pages(pages):
         content_html = open(page['filename']).read()
         template_html = open("../templates/base.html").read()
         template = Template(template_html)
-        template.render(
+        updated_page = template.render(
             title=page['title'],
             content=content_html,
         )
-    template = Template('''
+        open(page['output'], 'w+').write(updated_page)
+
+#This function loops through the navbar/index and creates an updated navbar based on the pages in the content folder.
+
+def build_index():
+    from jinja2 import Template
+    index_template = Template('''
     {% for page in pages %}
     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ page.output }}">{{ page.title }}</a></li>
     {% endfor %}
     ''')
-    template.render(pages=pages, content=content_html)
+    index_template.render(pages=pages, content=content_html)
+
+#This function creates a plain template for a new content page.
+
+def create_new():
+    new_page_template = '''
+    <h1>{{ New Content!}} </h1>
+    <p>{{ New content...}} </p>
+    '''
+    open('../content/new_content_page.html', 'w+').write(new_page_template)
 
 #The main function contains a loop that runs the two functions for each page, with the
 # result of updating each webpage.
